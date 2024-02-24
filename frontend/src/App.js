4d469a0fbd7f2b531e32f3f212463996c9a08295
Jason Cho <jyc24@njit.edu>
2024-02-24 16:31:51 -0500

    flask is working finally

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import { useState } from 'react'
 import TranslatePage from './components/translatepage/TranslatePage';
+import axios from "axios";
 import './App.css'; // Assuming you have global styles here
+import { FLASK_URL } from './vars.js'
 
 function App() {
+
+  const [profileData, setProfileData] = useState(null)
+  
+  function getData() {
+    axios({
+      method: "GET",
+      url:`${FLASK_URL}/profile`,
+    })
+    .then((response) => {
+      const res =response.data
+      console.log(res)
+      setProfileData(({
+        profile_name: res.name,
+        about_me: res.about}))
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+        }
+    })}
+
   return (
     <div className="App">
       <TranslatePage />
+
+      <p>To get your profile details: </p><button onClick={getData}>Click me</button>
+        {profileData && <div>
+              <p>Profile name: {profileData.profile_name}</p>
+              <p>About me: {profileData.about_me}</p>
+            </div>
+        }
     </div>
   );
 }
---
+++ frontend/src/vars.js
@@ -0,0 +1,1 @@
+export const FLASK_URL = "http://localhost:5000";
