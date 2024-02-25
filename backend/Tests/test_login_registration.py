e66a008aeff36a4e31cec75b9bfcd026ed227181
Jason Cho <jyc24@njit.edu>
2024-02-25 03:46:03 -0500

    added more unit tests- still some more  to go

--- backend/Tests/test_login_registration.py
+++ backend/Tests/test_login_registration.py
@@ -52,6 +52,7 @@ class TestLoginRegistrationTests:
         assert "emailErrors" not in response
         assert response["hasError"]
         assert "usernameErrors" in response
+        assert len(response["usernameErrors"]) == 1
 
         # test username too long
         response = client.post("/registerNewUser", data=json.dumps({"username": "sampleUsernameIsFarTooLongAndThrowsError", "email": valid_email, "password": valid_password}))
@@ -61,6 +62,7 @@ class TestLoginRegistrationTests:
         assert "emailErrors" not in response
         assert response["hasError"]
         assert "usernameErrors" in response
+        assert len(response["usernameErrors"]) == 1
 
         # test username uses no alphanumeric characters
         response = client.post("/registerNewUser", data=json.dumps({"username": "__------__", "email": valid_email, "password": valid_password}))
@@ -70,6 +72,7 @@ class TestLoginRegistrationTests:
         assert "emailErrors" not in response
         assert response["hasError"]
         assert "usernameErrors" in response
+        assert len(response["usernameErrors"]) == 1
 
         # test username does not follow username format
         response = client.post("/registerNewUser", data=json.dumps({"username": "this$is$bad$format", "email": valid_email, "password": valid_password}))
@@ -79,6 +82,48 @@ class TestLoginRegistrationTests:
         assert "emailErrors" not in response
         assert response["hasError"]
         assert "usernameErrors" in response
+        assert len(response["usernameErrors"]) == 1
 
     def test_user_registration_invalid_email_returns_error_response(self, client):
-        pass
+        valid_username = "sampleUser"
+        valid_password = "somepassword"
+
+        # test email length too long
+        response = client.post("/registerNewUser", data=json.dumps({"username": valid_username, "email": "this.email@is.wayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy.toooooooooo.long", "password": valid_password}))
+        response = response.json
+
+        assert "success" not in response
+        assert "usernameErrors" not in response
+        assert response["hasError"]
+        assert "emailErrors" in response
+        assert len(response["emailErrors"]) == 1
+
+        # test email invalid format 1
+        response = client.post("/registerNewUser", data=json.dumps({"username": valid_username, "email": "thisemail@usesinvalidformat", "password": valid_password}))
+        response = response.json
+
+        assert "success" not in response
+        assert "usernameErrors" not in response
+        assert response["hasError"]
+        assert "emailErrors" in response
+        assert len(response["emailErrors"]) == 1
+
+        # test email invalid format 2
+        response = client.post("/registerNewUser", data=json.dumps({"username": valid_username, "email": "thisemail.also@usesinvalid-format", "password": valid_password}))
+        response = response.json
+
+        assert "success" not in response
+        assert "usernameErrors" not in response
+        assert response["hasError"]
+        assert "emailErrors" in response
+        assert len(response["emailErrors"]) == 1
+
+        # test email invalid format 3
+        response = client.post("/registerNewUser", data=json.dumps({"username": valid_username, "email": "thisemail@usesinvalidformat..also", "password": valid_password}))
+        response = response.json
+
+        assert "success" not in response
+        assert "usernameErrors" not in response
+        assert response["hasError"]
+        assert "emailErrors" in response
+        assert len(response["emailErrors"]) == 1
