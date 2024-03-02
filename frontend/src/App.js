7d98c6ff595745c3fd45f1626bf049c1b60542cf
Dzejla Arapovic <da33@njit.edu>
2024-03-02 18:14:19 -0500

    pushing what i have for feedback form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      {/* <TranslatePage /> */}
-      <FeedbackForm />
+      <TranslatePage />
     </div>
   );
 }
--- frontend/src/components/FeedbackForm/FeedbackForm.jsx
+++ frontend/src/components/FeedbackForm/FeedbackForm.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
+import './FeedbackForm.css';
 import { FLASK_URL } from '../../vars.js'
 
 function FeedbackForm() {
   const [limit, setLimit] = useState(300);
   const [rating, setRating] = useState('');
   const [openended, setOpenEnded] = useState('');
-  
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -24,33 +24,250 @@ function FeedbackForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>1. The code translation was precise</label>
-        <select value={rating} onChange={(e) => setRating(e.target.value)}>
-          <option value="1">strongly disagree</option>
-          <option value="2">somewhat disagree</option>
-          <option value="3">neither agree or disagree</option>
-          <option value="4">somewhat agree</option>
-          <option value="5">strongly agree</option>
-        </select>
-      </div>
-      <div>
-        <label>Other:</label>
-        <textarea
-          value={openended}
-          onChange={handleChange}
-          placeholder={`Type here (Limit: ${limit} characters)`}
-          rows={4}
-          cols={50}
-          />
-        <div>
-          Character Count: {openended.length}/{limit}
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="background-page">
+            <h1>
+              User Feedback Form
+            </h1>
+            <h2>
+              Tell us how we're doing
+            </h2>
+          <div className="questions-container">
+            <label className="question">1. The code translation was precise</label>
+            <label className="question">2. The translator was easy to use</label>
+            <label className="question">3. The code translation process was quick</label>
+            <label className="question">4. I would use codeCraft for my coding projects again</label>
+          </div>
+            
+            <div className="radio-buttons">
+                <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Strongly Disagree
+                </label>
+                 <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Somewhat Disagree
+                </label>
+                 <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Neutral
+                </label>
+                 <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Somewhat Agree
+                </label>
+                 <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Strongly Agree
+                </label>
+            </div>
+            <div className="radio-buttons">
+                <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Strongly Disagree
+                </label>
+                 <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Somewhat Disagree
+                </label>
+                 <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Neutral
+                </label>
+                 <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Somewhat Agree
+                </label>
+                 <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Strongly Agree
+                </label>
+              </div>
+              <div className="radio-buttons">
+                <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Strongly Disagree
+                </label>
+                 <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Somewhat Disagree
+                </label>
+                 <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Neutral
+                </label>
+                 <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Somewhat Agree
+                </label>
+                 <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Strongly Agree
+                </label>
+              </div>
+              <div className="radio-buttons">
+                <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Strongly Disagree
+                </label>
+                 <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Somewhat Disagree
+                </label>
+                 <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Neutral
+                </label>
+                 <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Somewhat Agree
+                </label>
+                 <label>
+                  <input
+                    type="radio"
+                    name="question1"
+                    value="1"
+                    checked={rating === "1"}
+                    onChange={(e) => setRating(e.target.value)}
+                  />
+                  Strongly Agree
+                </label>
+              </div>
+
+          <div>
+            <label>Other:</label>
+            <textarea className = "textbox"
+              value={openended}
+              onChange={handleChange}
+              placeholder={`Type here (Limit: ${limit} characters)`}
+              rows={4}
+              cols={50}
+              />
+            <div>
+              Character Count: {openended.length}/{limit}
+            </div>
+          </div>
+          <button className = "submit-button" type="submit">Submit</button>
         </div>
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+      </form>
+    </div>
   );
 }
 
 export default FeedbackForm;
+
