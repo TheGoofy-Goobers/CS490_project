8a74c7c235d2652416c2688f33a02074f6040922
NamanRaval <nyr2@njit.edu>
2024-03-02 18:38:22 -0500

    Nav Bar integrated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import TranslatePage from './components/translatepage/TranslatePage';
 import './App.css'; // Assuming you have global styles here
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NavBar from './components/navbar/NavBar';
+import Home from './components/homepage/Home' 
 
 function App() {
   return (
     <div className="App">
-      <TranslatePage />
+      <NavBar />
+      <div className='routing'>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/translate' element={<TranslatePage />}/>
+          </Routes>
+      </div>
     </div>
   );
 }
