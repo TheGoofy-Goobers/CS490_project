470662ee0dd40b78a4b5c04d1d747d9500bd0b98
Dzejla Arapovic <da33@njit.edu>
2024-03-02 20:52:11 -0500

    functioning front end on feedbackform

--- frontend/src/components/FeedbackForm/FeedbackForm.jsx
+++ frontend/src/components/FeedbackForm/FeedbackForm.jsx
@@ -4,22 +4,38 @@ import { FLASK_URL } from '../../vars.js'
 
 function FeedbackForm() {
   const [limit, setLimit] = useState(300);
-  const [rating, setRating] = useState('');
+  const [ratings, setRatings] = useState({
+    question1: '',
+    question2: '',
+    question3: '',
+    question4: ''
+  });
   const [openended, setOpenEnded] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // reset
     setOpenEnded('');
-    setRating('');
+    setRatings({
+      question1: '',
+      question2: '',
+      question3: '',
+      question4: ''
+    });
     setLimit(300);
+  };
+
+  const handleRatingChange = (question, value) => {
+    setRatings(prevState => ({
+      ...prevState,
+      [question]: value
+    }));
   };
 
   const handleChange = (event) => {
     const inputValue = event.target.value;
-    if (inputValue.length <= limit){
+    if (inputValue.length <= limit) {
       setOpenEnded(inputValue);
-    // throw error
     }
   }
 
@@ -27,7 +43,7 @@ function FeedbackForm() {
     <div>
       <form onSubmit={handleSubmit}>
         <div className="background-page">
-          <div className = "title">
+          <div className="title">
             User Feedback Form
           </div>
           <div className="subtitle">
@@ -39,7 +55,7 @@ function FeedbackForm() {
             <label className="question">3. The code translation process was quick</label>
             <label className="question">4. I would use codeCraft for my coding projects again</label>
           </div>
-          <div className = "radio-buttons label">
+          <div className="radio-buttons label">
             <label>strongly disagree</label>
             <label>somewhat disagree</label>
             <label>neutral</label>
@@ -47,207 +63,72 @@ function FeedbackForm() {
             <label>strongly agree</label>
           </div>
           <div className="radio-buttons">
-            <label>
-              <input
-                type="radio"
-                name="question1"
-                value="1"
-                checked={rating === "1"}
-                onChange={(e) => setRating("1")}
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="question1"
-                value="1"
-                checked={rating === "2"}
-                onChange={(e) => setRating("2")}
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="question1"
-                value="1"
-                checked={rating === "3"}
-                onChange={(e) => setRating("3")}
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="question1"
-                value="1"
-                checked={rating === "4"}
-                onChange={(e) => setRating("4")}
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="question1"
-                value="1"
-                checked={rating === "5"}
-                onChange={(e) => setRating("5")}
-              />
-            </label>
+            {[1, 2, 3, 4, 5].map(value => (
+              <label key={value}>
+                <input
+                  type="radio"
+                  name="question1"
+                  value={value}
+                  checked={ratings['question1'] === value.toString()}
+                  onChange={() => handleRatingChange('question1', value.toString())}
+                />
+              </label>
+            ))}
           </div>
           <div className="radio-buttons">
-            <label>
-              <input
-                type="radio"
-                name="question2"
-                value="1"
-                checked={rating === "1"}
-                onChange={(e) => setRating("1")}
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="question2"
-                value="2"
-                checked={rating === "2"}
-                onChange={(e) => setRating("2")}
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="question2"
-                value="3"
-                checked={rating === "3"}
-                onChange={(e) => setRating("3")}
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="question2"
-                value="4"
-                checked={rating === "4"}
-                onChange={(e) => setRating("4")}
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="question2"
-                value="5"
-                checked={rating === "5"}
-                onChange={(e) => setRating("5")}
-              />
-            </label>
+            {[1, 2, 3, 4, 5].map(value => (
+              <label key={value}>
+                <input
+                  type="radio"
+                  name="question2"
+                  value={value}
+                  checked={ratings['question2'] === value.toString()}
+                  onChange={() => handleRatingChange('question2', value.toString())}
+                />
+              </label>
+            ))}
           </div>
           <div className="radio-buttons">
-            <label>
-              <input
-                type="radio"
-                name="question3"
-                value="1"
-                checked={rating === "1"}
-                onChange={(e) => setRating("1")}
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="question3"
-                value="2"
-                checked={rating === "2"}
-                onChange={(e) => setRating("2")}
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="question3"
-                value="3"
-                checked={rating === "3"}
-                onChange={(e) => setRating("3")}
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="question3"
-                value="4"
-                checked={rating === "4"}
-                onChange={(e) => setRating("4")}
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="question3"
-                value="5"
-                checked={rating === "5"}
-                onChange={(e) => setRating("5")}
-              />
-            </label>
+            {[1, 2, 3, 4, 5].map(value => (
+              <label key={value}>
+                <input
+                  type="radio"
+                  name="question3"
+                  value={value}
+                  checked={ratings['question3'] === value.toString()}
+                  onChange={() => handleRatingChange('question3', value.toString())}
+                />
+              </label>
+            ))}
           </div>
           <div className="radio-buttons">
-            <label>
-              <input
-                type="radio"
-                name="question4"
-                value="1"
-                checked={rating === "1"}
-                onChange={(e) => setRating("1")}
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="question4"
-                value="2"
-                checked={rating === "2"}
-                onChange={(e) => setRating("2")}
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="question4"
-                value="3"
-                checked={rating === "3"}
-                onChange={(e) => setRating("3")}
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="question4"
-                value="4"
-                checked={rating === "4"}
-                onChange={(e) => setRating("4")}
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="question4"
-                value="5"
-                checked={rating === "5"}
-                onChange={(e) => setRating("5")}
-              />
-            </label>
+            {[1, 2, 3, 4, 5].map(value => (
+              <label key={value}>
+                <input
+                  type="radio"
+                  name="question4"
+                  value={value}
+                  checked={ratings['question4'] === value.toString()}
+                  onChange={() => handleRatingChange('question4', value.toString())}
+                />
+              </label>
+            ))}
           </div>
-          <div className = "other-container">
+          <div className="other-container">
             <label>Other:</label>
-            <textarea className = "textbox"
+            <textarea
+              className="textbox"
               value={openended}
               onChange={handleChange}
               placeholder={`Type here (Limit: ${limit} characters)`}
               rows={4}
               cols={50}
-              />
+            />
             <div>
               Character Count: {openended.length}/{limit}
             </div>
           </div>
-          <button className = "submit-button" type="submit">Submit</button>
+          <button className="submit-button" type="submit">Submit</button>
         </div>
       </form>
     </div>
@@ -255,4 +136,3 @@ function FeedbackForm() {
 }
 
 export default FeedbackForm;
-
