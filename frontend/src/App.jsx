3066d1fe9415db6799b327db1d445390d41b70f8
Dzejla Arapovic <da33@njit.edu>
2024-03-02 21:13:00 -0500

    resloving merge

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import TranslatePage from './components/translatepage/TranslatePage.jsx';
+import NavBar from './components/navbar/NavBar';
+import Home from './components/homepage/Home' 
+import './App.css'; // Assuming you have global styles here
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginPage from './components/LoginPage/LoginPage.jsx';
+import Register from './components/Registration/RegistrationPage.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <div className='routing'>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/translate' element={<TranslatePage />}/>
+            <Route path='/login' element={<LoginPage />}/>
+            <Route path='/register' element={<Register />}/>
+          </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
