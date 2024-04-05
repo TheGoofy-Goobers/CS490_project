31de742a8d1f958776bb5914c549b711fc4bfb4c
Jason Cho <jyc24@njit.edu>
2024-04-05 14:19:57 -0400

    translation feedback backend and test handled w/ uuid

--- backend/functions/translation_feedback.py
+++ backend/functions/translation_feedback.py
@@ -1,28 +1,45 @@
 from flask_mysqldb import MySQL
 import json
 from flask import request
+from functions import get_user_id
 
-#TODO: THIS
 def submit_feedback(mysql: MySQL) -> dict:
     response = {"hasError": False}
 
+    responseJson = json.loads(request.data.decode())
+
+    if 'sessionToken' not in responseJson or 'star_rating' not in responseJson or 'note' not in responseJson:
+        response['hasError'] = True
+        response['errorMessage'] = "Unexpected Error"
+        return response
+    
+    user_id, error = get_user_id.get_user_id(mysql, responseJson['sessionToken'])
+    if error:
+        response['hasError'] = True
+        response['errorMessage'] = error
+        response['logout'] = True
+        return response
+
+    if user_id == -1:
+        response['hasError'] = True
+        response['errorMessage'] = "[LOGIN ERROR] User is not logged in!"
+        response['logout'] = True
+        return response
+    
+    star_rating = responseJson['star_rating']
+    note = responseJson['note'].strip()
+
+    if not star_rating in [1, 2, 3, 4, 5]:
+        response["hasError"] = True
+        response["errorMessage"] = "Invalid rating value(s)"
+        return response
+    
+    if not 0 <= len(note) <= 150:
+        response["hasError"] = True
+        response["errorMessage"] = "Invalid note string length"
+        return response
+    
     try:
-        responseJson = json.loads(request.data.decode())
-
-        user_id = responseJson['user_id']
-        star_rating = responseJson['star_rating']
-        note = responseJson['note'].strip()
-
-        if not star_rating in [1, 2, 3, 4, 5]:
-            response["hasError"] = True
-            response["errorMessage"] = "Invalid rating value(s)"
-            return response
-        
-        if not 0 <= len(note) <= 150:
-            response["hasError"] = True
-            response["errorMessage"] = "Invalid note string length"
-            return response
-        
         insertion = "INSERT INTO translation_feedback (user_id, star_rating, note) VALUES (%s, %s, %s)"
         cur = mysql.connection.cursor()
         cur.execute(insertion, (user_id, star_rating, note))
