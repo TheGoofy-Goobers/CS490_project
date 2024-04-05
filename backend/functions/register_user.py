c2cd5e588718fbc627bd9920165ef5c6d5f4ab6a
Jason Cho <jyc24@njit.edu>
2024-04-05 01:19:07 -0400

    fixed some things idk added uuid to frontend/backend for registration

--- backend/functions/register_user.py
+++ backend/functions/register_user.py
@@ -2,9 +2,11 @@ from flask_mysqldb import MySQL
 import json
 from flask import request
 from functions.validation import validate_username, validate_email
+import uuid
 
 def register(mysql: MySQL) -> dict:
     response = {"hasError" : False}
+    response["sqlErrors"] = []
 
     responseJson = json.loads(request.data.decode())
 
@@ -33,30 +35,62 @@ def register(mysql: MySQL) -> dict:
     if response["hasError"]:
         return response
     
-    # query db to make sure email and username are unique
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT username, email FROM users WHERE username = %s OR email = %s", (username, email))
-    existing_user = cur.fetchone()
+    try:
+        # query db to make sure email and username are unique
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT username, email FROM users WHERE username = %s OR email = %s", (username, email))
+        existing_user = cur.fetchone()
 
-    if existing_user:
+        if existing_user:
+            response["hasError"] = True
+            response["sqlErrors"] = []
+            if existing_user["username"] == username:
+                response["sqlErrors"].append("Chosen username already in use")
+            if existing_user["email"] == email:
+                response["sqlErrors"].append("Chosen email already in use")
+            return response
+    except Exception as e:
         response["hasError"] = True
-        response["sqlErrors"] = []
-        if existing_user["username"] == username:
-            response["sqlErrors"].append("Chosen username already in use")
-        if existing_user["email"] == email:
-            response["sqlErrors"].append("Chosen email already in use")
-        return response
-    
-    # insert new user into db
-    cur.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s)", (username, email, encrypted_pw))
-    mysql.connection.commit()
+        response["sqlErrors"].append(str(e))
+        cur.close()
 
-    # return user id
-    cur.execute("SELECT user_id FROM users WHERE username=%s", (username,))
-    user = cur.fetchone()
-    user_id = user["user_id"]
+    try:
+        # insert new user into db
+        cur.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s)", (username, email, encrypted_pw))
+        mysql.connection.commit()
+    except Exception as e:
+        mysql.connection.rollback()
+        response["hasError"] = True
+        response["sqlErrors"].append(str(e))
+        cur.close()
+
+    # get user id
+    user_id = ""
+    try:
+        cur.execute("SELECT user_id FROM users WHERE username=%s", (username,))
+        user = cur.fetchone()
+        if not user:
+            raise Exception("Username was not properly inserted into the database.") #Better hope we never hit this
+        user_id = user["user_id"]
+    except Exception as e:
+        cur.close()
+        response["hasError"] = True
+        response["sqlErrors"].append(str(e))
+
+    # insert user id into new table, also generate a uuid
+    id = str(uuid.uuid4())
+
+    try:
+        cur.execute("INSERT INTO logged_in(user_id, session_token) VALUES(%s, %s)", (user_id, id))
+        mysql.connection.commit()
+    except:
+        mysql.connection.rollback()
+        cur.close()
+        response["hasError"] = True
+        response["sqlErrors"].append(str(e))
+
     cur.close()
-
+    
     response["success"] = True
-    response["user_id"] = user_id
+    response["sessionToken"] = id
     return response
