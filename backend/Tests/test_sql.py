f0d7f018d24cefc08868daf53b1987f397aa6856
Jason Cho <jyc24@njit.edu>
2024-04-09 16:54:56 -0400

    added sql tests for delete account and translation history

--- backend/Tests/test_sql.py
+++ backend/Tests/test_sql.py
@@ -63,7 +63,7 @@ class TestSql:
         return app.test_cli_runner()
     
     @pytest.mark.parametrize("username,email,password,expectExisting", [("username1", "email@email.com", "password", True), ("unique", "email@email.com", "password", False)])
-    def test_registration_sql(self, username, email, password, expectExisting):
+    def test_registration_sql_and_delete_user(self, username, email, password, expectExisting):
         cur = connection.cursor(dictionary=True)
 
         try:
@@ -90,10 +90,25 @@ class TestSql:
             except Exception as e:
                 assert str(e) and False
 
+            # Test teardown- delete inserted value to prevent unexpected behavior in future tests - also delete user
+            try:
+                cur.execute("INSERT INTO logged_in(user_id, session_token) VALUES(%s, %s)", (4, str(uuid.uuid4())))
+                cur.execute("DELETE FROM users WHERE user_id = %s", (4,))
+                connection.commit()
+                cur.execute("SELECT * FROM logged_in")
+                user = cur.fetchone()
+                assert not user
+
+                cur.execute("SELECT * FROM users")
+                user = cur.fetchall()
+                assert user and len(user) == 3
+            except Exception as e:
+                assert str(e) and False
+
         cur.close()
 
     @pytest.mark.parametrize("username,password,expectExisting", [("username", "password", False), ("username1", "password", True), ("email1@email.com", "password", True)])
-    def test_login_sql(self, username, password, expectExisting): # This test should cover the login portion of user registration as well
+    def test_login_sql_and_get_user_id(self, username, password, expectExisting): # This test should cover the login portion of user registration as well
         cur = connection.cursor(dictionary=True)
 
         user = None
@@ -118,7 +133,7 @@ class TestSql:
                 cur.execute("SELECT * FROM logged_in WHERE user_id = %s", (user["user_id"],))
                 user = cur.fetchone()
                 assert user
-                assert user["session_token"] == id
+                assert user["session_token"] == id # This should be sufficient for ensuring get_user_id works
 
                 connection.commit()
                 cur.execute("SELECT * FROM logged_in WHERE user_id = %s", (user["user_id"],))
@@ -136,4 +151,23 @@ class TestSql:
             except Exception as e:
                 assert str(e) and False
 
+        cur.close()
+    
+    @pytest.mark.parametrize("user_id", [(1)])
+    def test_translation_history_sql(self, user_id):
+        cur = connection.cursor(dictionary=True)
+
+        rows = None
+        try:
+            cur.execute("SELECT source_language, original_code, target_language, translated_code, submission_date FROM translation_history WHERE user_id=%s ORDER BY submission_date DESC", (user_id,))
+            rows = cur.fetchall()
+            assert rows
+        except Exception as e:
+            assert str(e) and False
+        
+        assert len(rows) == 3
+        assert rows[0]["source_language"] == "python"
+        assert rows[1]["original_code"] == "print('Hello world 1!')"
+        assert rows[2]["target_language"] == "javascript"
+        assert rows[0]["translated_code"] == "console.log('Hello world 0!');"
         cur.close()
