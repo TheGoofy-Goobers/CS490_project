76bdc342edc9b771843a1c7f61a126362bcba132
Hamdi-korreshi <hamdikorreshi256@gmail.com>
2024-04-22 13:16:35 -0400

    starting release notes

---
+++ frontend/src/components/ReleaseNotes/ReleaseNotes.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './report.css';
+import { FLASK_URL, SITE_URL } from '../../vars.js';
+import axios from 'axios';
+
+function Report() {
+    return (
+        <div>
+            <p>Pending release</p>
+        </div>
+    )
+}
