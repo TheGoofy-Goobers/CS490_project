5b4535c109d9c4946da9aec5d3dcab376c7ff4d7
Dzejla Arapovic <da33@njit.edu>
2024-04-23 15:43:53 -0400

    home page now has average star ratings

--- frontend/src/components/homepage/Home.jsx
+++ frontend/src/components/homepage/Home.jsx
@@ -1,65 +1,103 @@
 import React from "react";
-import logo3 from '../navbar/logo3.png'
+import logo3 from '../navbar/logo3.png';
 import './Home.css';
-import arrow from './right arrow orange.png'
-import javascr from './Image 9.png'
-import py from './Image 10.png'
-import cprog from './Image 11.png'
-import jav from './Image 12.png'
+import arrow from './right arrow orange.png';
+import javascr from './Image 9.png';
+import py from './Image 10.png';
+import cprog from './Image 11.png';
+import jav from './Image 12.png';
 import { Link } from 'react-router-dom';
-import paste from './computer-icons-clipboard-cut-copy-and-paste-symbol-removebg-preview.png'
-import congrats from './confetti.png'
+import paste from './computer-icons-clipboard-cut-copy-and-paste-symbol-removebg-preview.png';
+import congrats from './confetti.png';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { FLASK_URL, setLocal, isExpired, Logout, isLoggedIn } from '../../vars';
+import halfstar from './halfstar.png';
+import star from './fullstar.png';
+import emptystar from './emptystar.png';
+
 
 const Home = () => {
+
+    const [rating, setRating] = useState('');
+    const [totalRatings, setTotalRatings] = useState();
+
+    useEffect(() => {
+        fetchRating();
+    }, []);
+
+    const fetchRating = () => {
+        axios.get(`${FLASK_URL}/getAggregatedFeedback`)
+            .then(response => {
+                console.log('Verification response:', response.data);
+                const res = response.data;
+                if (res.success) {
+                    setRating(res.average_rating);
+                    console.log(`rating ${res.average_rating}`);
+                    setTotalRatings(res.total_ratings);
+                }
+                if (res.hasError) console.log(`Error response: ${res.errorMessage}`);
+                console.log(`Response has error: ${res.hasError}`);
+                if (res.logout) {
+                    setTimeout(Logout, 4000);
+                }
+            })
+            .catch(error => {
+                console.error('Verification error:', error);
+                // TODO: failed verification message
+            });
+    };
+
+    const renderStars = () => {
+        const stars = [];
+        const maxRating = 5; // Maximum rating possible
+        const fullStars = Math.floor(rating); // Number of full stars
+        const hasHalfStar = rating % 1 >= 0.5; // Check if there should be a half star
+
+        for (let i = 0; i < maxRating; i++) {
+            if (i < fullStars) {
+                // Replace 'fullStar.png' with the path to your full star image
+                stars.push(<img key={i} src={star} alt="Full Star" className="star" />);
+            } else if (i === fullStars && hasHalfStar) {
+                // Replace 'halfStar.png' with the path to your half star image
+                stars.push(<img key={i} src={halfstar} alt="Half Star" className="halfstar" />);
+            } else {
+                // Optionally, have an 'emptyStar.png' or just keep using the Unicode character
+                stars.push(<img key={i} src={emptystar} alt="Empty Star" className="star" />);
+            }
+        }
+        return stars;
+    };
+
     return (
         <div className="back">
             <div>
-            <img className="title" src={logo3}/>
+                <img className="title" src={logo3} />
 
-            <div className="intro">
-            <h2>codeCraft is where all your code translating needs are met!</h2>
-            <h2>Powered by the new ChatGPT-3.5 and fine tuned by us to accomplish all your coding needs.</h2>
+                <div className="intro">
+                    <h2>codeCraft is where all your code translating needs are met!</h2>
+                    <h2>Powered by the new ChatGPT-3.5 and fine tuned by us to accomplish all your coding needs.</h2>
+                </div>
             </div>
-
-            
-            <h1 className="four">4 Simple Steps to Get Started</h1>
-            <div className="steps">
+            <div className="reviews">
+                <h2>How our users    <span className="rating-number">   ({totalRatings})</span> rate us:</h2>
+                {renderStars()}
                 
-                <div className="step1">
-                    <h3>Choose Input and Output Language</h3>
-                    <div className="langholder">
-                        <img id='langs' src={javascr}/>
-                        <img id='langs2' src={cprog}/>
-                    </div>
-                </div>
-                <img className='arrows' src={arrow}/>
-                <div className="step2">
-                    <h3>Paste in your Code</h3>
-                    <img className='paste' src={paste}/>
-                </div>
-                <img className='arrows' src={arrow}/>
-                <div className="step3">
-                    <h3>ALL DONE!!! 
-                        ENJOY YOUR NEW CODE</h3>
-                    <img className="finished" src={congrats}/>
-                </div>
             </div>
-            </div>
-            
             <h1>Accepted Languages</h1>
             <div className="Accepted">
-                <img className="langs" src={javascr}/>
-                <img className="langs" src={py}/>
-                <img className="langs" src={cprog}/>
-                <img className="langs" id="jav" src={jav}/>
+                <img className="langs" src={javascr} />
+                <img className="langs" src={py} />
+                <img className="langs" src={cprog} />
+                <img className="langs" id="jav" src={jav} />
             </div>
             <div className="box">
                 <Link to="/translate">
-                <button className="translate_now" >Try it out !!!</button>
+                    <button className="translate_now" >Try it out !!!</button>
                 </Link>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Home;
